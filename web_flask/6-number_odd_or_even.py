f8a8af09b7841daa0cd5f8c67d2d0049ead3160c
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-22 16:36:41 +0300

    task 5 6 :

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def is_number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """number_template -> 5-number.html
     """
